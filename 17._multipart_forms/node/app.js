1cc6d99098ac56ed438bef1119917274d298520e
raskoeStud <rasm546d@stud.kea.dk>
2023-05-09 18:14:17 +0200

    updated multipart forms

--- 17._multipart_forms/node/app.js
+++ 17._multipart_forms/node/app.js
@@ -1,20 +1,42 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-import cors from 'cors';
-app.use(cors());
+import multer from "multer";
+// const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        const filenameParts = file.originalname.split(".");
+        if (filenameParts.length <= 1) {
+            cb(new Error("File has no extension: "  + file.originalname));
+        }
 
-app.get('/', (req, res) => {
-    res.send("Welcome to form page");
+        const extension = filenameParts.pop();
+        const originalFilename = filenameParts.join(".");
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+
+        const newFileName = uniqueSuffix + "___" + originalFilename + "." + extension;
+
+        cb(null, newFileName);
+    }
+});
+const upload = multer({ storage });
+
+
+app.post("/form", (req, res) => {
+    console.log(req.body);
+    delete req.body.password;
+    res.send({ data: req.body });
 });
 
-app.post('/form', (req, res) => {
+app.post("/fileform", upload.single("file"), (req, res) => {
+    console.log(req.file);
     res.send({ data: req.body })
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log("Server is running in port", PORT));
